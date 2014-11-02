b62704a685f33d2188e5e21b6bdd648b55efc354
Ramon Invarato <r.invarato@gmail.com>
2014-11-02 18:57:20 +0100

    Primera versión del código

---
+++ FragmentEstaticosDiferentesPantallas/src/com/jarroba/fragmentestaticosdiferentespantallas/MainActivity.java
@@ -0,0 +1,19 @@
+package com.jarroba.fragmentestaticosdiferentespantallas;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Código del libro Android 100% para <a href="http://jarroba.com/">www.jarroba.com</a>. Descarga el libro de Android 100% gratis desde <a href="http://jarroba.com/libro-android-100-gratis/">www.jarroba.com/libro-android-100-gratis</a>
+ * 
+ * @author Ramón Invarato Menéndez
+ */
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+
+}
